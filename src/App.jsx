4df563aac6f679ad0d1a8522961e5f476782fbe6
Amina <113165708+asmina18@users.noreply.github.com>
2023-08-29 12:08:23 +0200

    OpgaveFetch

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [joke, setJoke] = useState('');
+
+  const fetchJoke = () => {
+    const url = 'https://official-joke-api.appspot.com/random_joke';
+
+    fetch(url)
+      .then(res=> res.json())
+      .then(data => {
+
+        const jokeText = `${data.setup} ${data.punchline}`; 
+        setJoke(jokeText);
+      })
+      
+  };
+
+  useEffect(() => {
+    fetchJoke(); 
+  }, []);
+
+  console.log('Joke', joke);
+
+  return (
+    <div className="App">
+      <h1>Joke App</h1>
+      <p>{joke}</p>
+      <button onClick={fetchJoke}>Get Joke</button>
+    </div>
+  );
+}
+
+export default App;
